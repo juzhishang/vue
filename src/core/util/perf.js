2610f9e10bda260d4b41b9794d8764323206fd32
juzhishang <793821516@qq.com>
2018-07-09 17:00:48 +0800

    docs

--- src/core/util/perf.js
+++ src/core/util/perf.js
@@ -2,7 +2,14 @@ import { inBrowser } from './env'
 
 export let mark
 export let measure
-
+// 非生产环境下如果浏览器支持performance，封装performance.mark和performance.measure
+/**
+ * 用法
+ * mark('markStartName')
+ * // somecode...
+ * mark('markEndName')
+ * measure('measureName', 'markStartName', 'markEndName')
+ */
 if (process.env.NODE_ENV !== 'production') {
   const perf = inBrowser && window.performance
   /* istanbul ignore if */
