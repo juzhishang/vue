b89dc23f3944f1ec5250b0233a0b22d9b422c79c
juzhishang <793821516@qq.com>
2018-07-09 17:59:36 +0800

    parsePath挺有意思的

--- src/core/util/lang.js
+++ src/core/util/lang.js
@@ -2,6 +2,7 @@
 
 /**
  * Check if a string starts with $ or _
+ * 是否预留，检测字符串是否以$或_开头
  */
 export function isReserved (str: string): boolean {
   const c = (str + '').charCodeAt(0)
@@ -10,6 +11,7 @@ export function isReserved (str: string): boolean {
 
 /**
  * Define a property.
+ * 封装了一下Object.defineProperty
  */
 export function def (obj: Object, key: string, val: any, enumerable?: boolean) {
   Object.defineProperty(obj, key, {
@@ -23,7 +25,10 @@ export function def (obj: Object, key: string, val: any, enumerable?: boolean) {
 /**
  * Parse simple path.
  */
+// \w: 表示匹配包括下划线的任何单词字符。类似但不等价于“[A-Za-z0-9_]”，这里的"单词"字符使用Unicode字符集。
+// 匹配不包含\w.$的字符
 const bailRE = /[^\w.$]/
+// parsePath('a.b.c')({ a: { b: { c: 'hello' } } }) ,会返回'hello'
 export function parsePath (path: string): any {
   if (bailRE.test(path)) {
     return
